d31d7bc986a81febe79a7b7216614e7c954b2471
19mdavenport <mdaven19@byu.edu>
2023-08-11 13:32:20 -0600

    update test factory

--- chess/1-chess-game/starter-code/passoffTests/TestFactory.java
+++ chess/1-chess-game/starter-code/passoffTests/TestFactory.java
@@ -52,10 +52,11 @@ public class TestFactory {
     public static Long getMessageTime(){
         /*
         Changing this will change how long tests will wait for the server to send messages.
-        3 Seconds will be enough for most computers. Feel free to change as you see fit, just know increasing it can
-        make tests take longer to run. (On the flip side, if you've got a good computer feel free to decrease it)
+        3000 Milliseconds (3 seconds) will be enough for most computers. Feel free to change as you see fit,
+        just know increasing it can make tests take longer to run.
+        (On the flip side, if you've got a good computer feel free to decrease it)
          */
-        return 3L;
+        return 3000L;
     }
     //------------------------------------------------------------------------------------------------------------------
 }
