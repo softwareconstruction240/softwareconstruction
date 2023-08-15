2f23b4589fe08f2b3c3b2fd52f380f61ed8e09c8
rodham <ken.rodham@gmail.com>
2023-08-15 15:35:20 -0600

    Updated the Console UI instruction.

--- instruction/console-ui/console-ui.md
+++ instruction/console-ui/console-ui.md
@@ -1,22 +1,24 @@
-# Title
+# Console User Interface (UI)
 
 🖥️ [Slides]()
 
 📖 **Required Reading**: Core Java SE 9 for the Impatient
 
-- Chapter x:
+- None
 
-blurb
+Typically, when a program prints output to the console (or terminal), the text plain without much formatting. However, it is possible to create fairly nice console user interfaces that include formatted and colored text and use character-based graphics. This lecture explains how to create
+a text-based console user interface, with an emphasis on how to implement the Chess client user interface.
+
+![Console User Interface](Midnight_Commander_(2005)_en.png)
+
 
 ## Things to Understand
 
-- x
-- x
-
-## Videos
-
-- 🎥 [x]()
+- How to use terminal control codes to clear the terminal, set background and text colors, and text attributes such as bold, faint, italic, underline, and blinking
+- How to use Unicode characters to draw the chess board and pieces for the Chess project
 
 ## Demonstration code
 
-📁 [x](example-code/)
+📁 [Escape Sequences](example-code/src/ui/EscapeSequences.java)
+
+📁 [Tic-Tac-Toe](example-code/src/ui/TicTacToe.java)
---
+++ instruction/console-ui/example-code/src/ui/TicTacToe.java
@@ -0,0 +1,148 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+import static ui.EscapeSequences.*;
+
+public class TicTacToe {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 3;
+    private static final int SQUARE_SIZE_IN_CHARS = 3;
+    private static final int LINE_WIDTH_IN_CHARS = 1;
+    private static final String EMPTY = "   ";
+    private static final String X = " X ";
+    private static final String O = " O ";
+    private static Random rand = new Random();
+
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out);
+
+        drawTicTacToeBoard(out);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void drawHeaders(PrintStream out) {
+
+        setBlack(out);
+
+        String[] headers = { "TIC", "TAC", "TOE" };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeader(out, headers[boardCol]);
+
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
+            }
+        }
+
+        out.println();
+    }
+
+    private static void drawHeader(PrintStream out, String headerText) {
+        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
+        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderText(out, headerText);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderText(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+
+        out.print(player);
+
+        setBlack(out);
+    }
+
+    private static void drawTicTacToeBoard(PrintStream out) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+
+            drawRowOfSquares(out);
+
+            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+                drawVerticalLine(out);
+                setBlack(out);
+            }
+        }
+    }
+
+    private static void drawRowOfSquares(PrintStream out) {
+
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_CHARS; ++squareRow) {
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                setWhite(out);
+
+                if (squareRow == SQUARE_SIZE_IN_CHARS / 2) {
+                    int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
+                    int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
+
+                    out.print(EMPTY.repeat(prefixLength));
+                    printPlayer(out, rand.nextBoolean() ? X : O);
+                    out.print(EMPTY.repeat(suffixLength));
+                }
+                else {
+                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_CHARS));
+                }
+
+                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                    // Draw right line
+                    setRed(out);
+                    out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
+                }
+
+                setBlack(out);
+            }
+
+            out.println();
+        }
+    }
+
+    private static void drawVerticalLine(PrintStream out) {
+
+        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
+                                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
+
+        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
+            setRed(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+
+            setBlack(out);
+            out.println();
+        }
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void printPlayer(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        out.print(player);
+
+        setWhite(out);
+    }
+}
