a3d64752f900deb8e0a05ee872bebb1e5a1f3875
Lee <lee.jensen@agilix.com>
2023-09-08 13:56:39 -0600

    update(spellchecker) to clarify modifications to the interface

--- spelling-corrector/starter-code/spell/Main.java
+++ spelling-corrector/starter-code/spell/Main.java
@@ -3,31 +3,32 @@ package spell;
 import java.io.IOException;
 
 /**
- * A simple main class for running the spelling corrector. This class is not
- * used by the passoff program.
+ * A main class for providing an entry point to your spelling corrector. This
+ * class is not used by the passoff program. However, you will need to alter
+ * this code to reference your SpellCorrector implementation.
  */
 public class Main {
-	
+
 	/**
 	 * Give the dictionary file name as the first argument and the word to correct
 	 * as the second argument.
 	 */
 	public static void main(String[] args) throws IOException {
-		
+
 		String dictionaryFileName = args[0];
 		String inputWord = args[1];
-		
+
 		//
-        //Create an instance of your corrector here
-        //
+		// Create an instance of your corrector here
+		//
 		ISpellCorrector corrector = null;
-		
+
 		corrector.useDictionary(dictionaryFileName);
 		String suggestion = corrector.suggestSimilarWord(inputWord);
 		if (suggestion == null) {
-		    suggestion = "No similar word found";
+			suggestion = "No similar word found";
 		}
-		
+
 		System.out.println("Suggestion is: " + suggestion);
 	}
 
