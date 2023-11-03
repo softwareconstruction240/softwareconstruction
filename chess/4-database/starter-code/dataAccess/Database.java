ebe77211337604c7bae3ad65f8c1b8a0ed84abf7
jerod.wilkerson <30474318+jerodw@users.noreply.github.com>
2023-11-03 17:47:52 -0600

    Changed the Database class to a simple implementation that can be used to get and close connections

--- chess/4-database/starter-code/dataAccess/Database.java
+++ chess/4-database/starter-code/dataAccess/Database.java
@@ -5,95 +5,45 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Class responsible for creating connections to the database
+ * Responsible for creating connections to the database. Connections should be closed after use, either by calling
+ * {@link #closeConnection(Connection)} on the Database instance or directly on the connection.
  */
 public class Database {
 
-    //FIXME replace with your database URL
-    //private static final String MYSQL_URL = "localhost:3306/MyAwesomeChessDatabase";
-    private static final String MYSQL_URL = "localhost:3306/chess";
-
-    //FIXME replace with your database username
-    //private static final String DB_USERNAME = "AwesomeChessServer";
+    // FIXME: Change these fields, if necessary, to match your database configuration
+    public static final String DB_NAME = "chess";
     private static final String DB_USERNAME = "root";
-
-    //FIXME replace with your database password (null if no password)
-    //private static final String DB_PASSWORD = null;
     private static final String DB_PASSWORD = "admin";
 
-    /**
-     * The connection to the database
-     */
-    private static Connection conn;
-    private static final String DATABASE_DRIVER = "jdbc:mysql://";
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
 
     /**
-     * Initiates a connection to the database
-     * @return new Connection object
-     * @throws DataAccessException
-     */
-    public Connection openConnection() throws DataAccessException {
-        try {
-            //shouldn't try to open an active connection
-            if (conn != null){
-                throw new DataAccessException("Database connection already open");
-            }
-
-            //The Structure for this Connection is driver:language:path
-            //The path assumes you start in the root of your project unless given a non-relative path
-            final String CONNECTION_URL = DATABASE_DRIVER + MYSQL_URL;
-
-            // Open a database connection to the file given in the path
-            conn = DriverManager.getConnection(CONNECTION_URL, DB_USERNAME, DB_PASSWORD);
-
-            // Start a transaction
-            conn.setAutoCommit(false);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new DataAccessException("Unable to open connection to database");
-        }
-
-        return conn;
-    }
-
-    /**
-     * If a connection exists, opens a connection. Otherwise, just uses current connection
-     * @return Connection to database
-     * @throws DataAccessException
+     * Gets a connection to the database.
+     *
+     * @return Connection the connection.
+     * @throws DataAccessException if a data access error occurs.
      */
     public Connection getConnection() throws DataAccessException {
-        if (conn == null) {
-            return openConnection();
-        } else {
-            return conn;
+        try {
+            return DriverManager.getConnection(CONNECTION_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
         }
     }
 
     /**
-     * Closes connection to database.
-     * @param commit If we want to commit changes to the database. If false, no changes will be made
-     * @throws DataAccessException
+     * Closes the specified connection.
+     *
+     * @param connection the connection to be closed.
+     * @throws DataAccessException if a data access error occurs while closing the connection.
      */
-    public void closeConnection(boolean commit) {
-        if (conn == null) {
-            return;
-        }
-
-        try {
-            if (commit) {
-                //This will commit the changes to the database
-                conn.commit();
-            } else {
-                //If we find out something went wrong, pass a false into closeConnection and this
-                //will rollback any changes we made during this connection
-                conn.rollback();
+    public void closeConnection(Connection connection) throws DataAccessException {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
             }
-
-            conn.close();
-            conn = null;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
