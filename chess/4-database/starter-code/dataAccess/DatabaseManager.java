bd4c83d68af50a07a43ba92375fbb3ffa3462dee
colbycade <colbycade@gmail.com>
2024-02-29 16:08:13 -0700

    Update DatabaseManager.java
    typo

--- chess/4-database/starter-code/dataAccess/DatabaseManager.java
+++ chess/4-database/starter-code/dataAccess/DatabaseManager.java
@@ -15,7 +15,7 @@ public class DatabaseManager {
     static {
         try {
             try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
-                if (propStream == null) throw new Exception("Unable to laod db.properties");
+                if (propStream == null) throw new Exception("Unable to load db.properties");
                 Properties props = new Properties();
                 props.load(propStream);
                 databaseName = props.getProperty("db.name");
