51913a5bc29ed31a21e387eff39210fbd87e041e
21aslade <andrewmslade@gmail.com>
2024-11-23 13:18:53 -0700

    enhance(petshop) use HttpClient instead of HttpURLConnection

--- petshop/shared/src/main/server/ServerFacade.java
+++ petshop/shared/src/main/server/ServerFacade.java
@@ -5,17 +5,19 @@ import exception.ErrorResponse;
 import exception.ResponseException;
 import model.Pet;
 
-import java.io.*;
 import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ServerFacade {
-
+    private final HttpClient client = HttpClient.newHttpClient();
     private final String serverUrl;
 
     public ServerFacade(String url) {
         serverUrl = url;
     }
-
 
     public Pet addPet(Pet pet) throws ResponseException {
         var path = "/pet";
@@ -40,17 +42,12 @@ public class ServerFacade {
         return response.pet();
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+    private <T> T makeRequest(String method, String path, Object body, Class<T> responseClass)
+            throws ResponseException {
         try {
-            URL url = (new URI(serverUrl + path)).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.setDoOutput(true);
-
-            writeBody(request, http);
-            http.connect();
-            throwIfNotSuccessful(http);
-            return readBody(http, responseClass);
+            var request = buildRequest(method, path, body);
+            var response = client.send(request, BodyHandlers.ofString());
+            return handleResponse(response, responseClass);
         } catch (ResponseException ex) {
             throw ex;
         } catch (Exception ex) {
@@ -58,43 +55,41 @@ public class ServerFacade {
         }
     }
 
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
 
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+    private BodyPublisher makeRequestBody(Object request) {
         if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
-            String reqData = new Gson().toJson(request);
-            try (OutputStream reqBody = http.getOutputStream()) {
-                reqBody.write(reqData.getBytes());
-            }
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
         }
     }
 
-    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
-        var status = http.getResponseCode();
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
         if (!isSuccessful(status)) {
-            try (InputStream respErr = http.getErrorStream()) {
-                if (respErr != null) {
-                    throw ResponseException.fromJson(respErr);
-                }
+            var body = response.body();
+            if (body != null) {
+                throw new Gson().fromJson(response.body(), ResponseException.class);
             }
 
             throw new ResponseException(status, "other failure: " + status);
         }
-    }
 
-    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
-        T response = null;
-        if (http.getContentLength() < 0) {
-            try (InputStream respBody = http.getInputStream()) {
-                InputStreamReader reader = new InputStreamReader(respBody);
-                if (responseClass != null) {
-                    response = new Gson().fromJson(reader, responseClass);
-                }
-            }
+        if (responseClass == null) {
+            return null;
         }
-        return response;
-    }
 
+        return new Gson().fromJson(response.body(), responseClass);
+    }
 
     private boolean isSuccessful(int status) {
         return status / 100 == 2;
