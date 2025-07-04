c19f29806604090bd23be1c8cd7458e915f6cf95
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 12:16:08 -0600

    Made MarkdownFile subclass FilePath

--- .github/structure.py
+++ .github/structure.py
@@ -2,17 +2,11 @@ from dataclasses import dataclass
 import os
 from typing import List
 
-@dataclass
-class MarkdownFile:
-  parent: str
-  filename: str
-  body: List[str]
-
 
 @dataclass(init=False)
 class FilePath:
   full_path: str
-  """Relative full path from root to the file"""
+  """Full path from root to the file"""
   parent: str
   """Immediate parent directory"""
   filename: str
@@ -22,3 +16,13 @@ class FilePath:
     self.filename = filename
     self.parent = os.path.basename(from_root)
     self.full_path = os.path.join(from_root, filename)
+
+
+@dataclass(init=False)
+class MarkdownFile(FilePath):
+  body: List[str]
+  """All content of the file as separate lines"""
+
+  def __init__(self, from_root: str, filename: str, body: List[str]):
+    super().__init__(from_root, filename)
+    self.body = body
