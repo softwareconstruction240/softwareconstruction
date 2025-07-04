de9e079327c231966b48a960a477c784878ab671
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 13:18:41 -0600

    Made dirpath not private

--- .github/structure.py
+++ .github/structure.py
@@ -5,7 +5,7 @@ from typing import List
 
 @dataclass
 class FilePath:
-    _dirpath: str
+    dirpath: str
     """Path from root to parent directory of file"""
     filename: str
     """Name of the file"""
@@ -13,12 +13,12 @@ class FilePath:
     @property
     def full_path(self) -> str:
         """Full path from root to the file"""
-        return os.path.join(self._dirpath, self.filename)
+        return os.path.join(self.dirpath, self.filename)
 
     @property
     def parent(self) -> str:
         """Immediate parent directory"""
-        return os.path.basename(self._dirpath)
+        return os.path.basename(self.dirpath)
 
     def relative_from(self, root: str):
         return os.path.relpath(self.full_path, root)
@@ -29,6 +29,6 @@ class MarkdownFile(FilePath):
     body: List[str]
     """All content of the file as separate lines"""
 
-    def __init__(self, from_root: str, filename: str, body: List[str]):
-        super().__init__(from_root, filename)
+    def __init__(self, dirpath: str, filename: str, body: List[str]):
+        super().__init__(dirpath, filename)
         self.body = body
