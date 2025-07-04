67329104bc550675bd880bb5be00bff34c701e78
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 14:20:38 -0600

    Fixed markdown extension

--- .github/rewrite.py
+++ .github/rewrite.py
@@ -55,7 +55,7 @@ def phase_prefix(path: str) -> str:
 
 def main(root: str, code_base: str):
     mapping: dict[str, MarkdownFile] = {}
-    for file_path in find_files_with_exts(root, ".md", root):
+    for file_path in find_files_with_exts(root, "md", root):
         title, body = extract_title_and_body(file_path.full_path)
         filename = file_path.filename
         if title:
