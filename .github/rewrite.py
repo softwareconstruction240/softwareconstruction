f3fb82a3bc039e3ab89e275d5a1f99bc2accb941
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 16:20:39 -0600

    Returned name map for markdown

--- .github/rewrite.py
+++ .github/rewrite.py
@@ -62,6 +62,12 @@ def main(root: str, code_base: str):
         mapping[file_path.full_path] = MarkdownFile(file_path.dirpath, filename, body)
         md_tuple_map[(file_path.parent, file_path.filename)] = file_path.filename
 
+    md_name_map: dict[str, str] = {}
+    for old_path, info in mapping.items():
+        old_filename = os.path.basename(old_path)
+        if old_filename not in md_name_map or md_name_map[old_filename] == info.filename:
+            md_name_map[old_filename] = info.filename
+
     embed_tuple_map = {(file_path.parent, file_path.filename): file_path.relative_from(root)
                        for file_path in find_files_with_exts(root, *EMBED_EXTS)}
 
@@ -96,7 +102,8 @@ def main(root: str, code_base: str):
 
             elif ext == 'md':
                 new_base = (md_tuple_map.get((dirname, basename))
-                    or md_tuple_map.get((info.parent, basename)))
+                    or md_tuple_map.get((info.parent, basename))
+                    or md_name_map.get(basename))
                 if new_base:
                     new_link = re.sub(r'\.md$', '', new_base, flags=re.IGNORECASE)
 
