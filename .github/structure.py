c67ef3741b89056faed744425a3a6a6eda0f0064
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 12:12:22 -0600

    Created second structure class, FilePath

--- .github/structure.py
+++ .github/structure.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import os
 from typing import List
 
 @dataclass
@@ -6,3 +7,18 @@ class MarkdownFile:
   parent: str
   filename: str
   body: List[str]
+
+
+@dataclass(init=False)
+class FilePath:
+  full_path: str
+  """Relative full path from root to the file"""
+  parent: str
+  """Immediate parent directory"""
+  filename: str
+  """Name of the file"""
+
+  def __init__(self, from_root: str, filename: str):
+    self.filename = filename
+    self.parent = os.path.basename(from_root)
+    self.full_path = os.path.join(from_root, filename)
