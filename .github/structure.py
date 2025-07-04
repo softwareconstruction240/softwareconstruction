005e8b659dc91f77eba4ebb9d1e29a34a97a264f
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-04 12:53:11 -0600

    Changed dataclass to use properties and store dirpath

--- .github/structure.py
+++ .github/structure.py
@@ -3,19 +3,25 @@ import os
 from typing import List
 
 
-@dataclass(init=False)
+@dataclass
 class FilePath:
-  full_path: str
-  """Full path from root to the file"""
-  parent: str
-  """Immediate parent directory"""
+  _dirpath: str
+  """Path from root to parent directory of file"""
   filename: str
   """Name of the file"""
-
-  def __init__(self, from_root: str, filename: str):
-    self.filename = filename
-    self.parent = os.path.basename(from_root)
-    self.full_path = os.path.join(from_root, filename)
+  
+  @property
+  def full_path(self) -> str:
+    """Full path from root to the file"""
+    return os.path.join(self._dirpath, self.filename)
+  
+  @property
+  def parent(self) -> str:
+    """Immediate parent directory"""
+    return os.path.basename(self._dirpath)
+  
+  def relative_from(self, root: str):
+    return os.path.relpath(self.full_path, root)
 
 
 @dataclass(init=False)
