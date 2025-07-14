b853342d1455dc9615b3b04fcd58a3571b6d0ed5
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-07-14 14:22:12 -0600

    Added more useful prints than previous

--- .github/rewrite.py
+++ .github/rewrite.py
@@ -107,6 +107,10 @@ def main(root: str, code_base: str):
                     new_link = re.sub(r'\.md$', '', new_base, flags=re.IGNORECASE)
 
             elif ext != '' or sep == '':
+                print(path_part)
+                if ext == '':
+                    print('Here due to sep')
+
                 abs_path = os.path.normpath(os.path.join(info.dirpath, path_part))
                 rel_to_root = os.path.relpath(abs_path, root)
 
