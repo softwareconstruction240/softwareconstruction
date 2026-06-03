d200a3cad1c9d57f4672071acee03506902d998b
Lee S Jensen <leesjensen@users.noreply.github.com>
2026-06-03 16:14:09 -0600

    remove(topic) Instruction topics

--- instruction/exceptions/example-code/ExceptionThrowingExample.java
+++
@@ -1,8 +0,0 @@
-public class ExceptionThrowingExample {
-
-    public void myMethod(int methodParam) {
-        if(methodParam < 0) {
-            throw new IllegalArgumentException("The parameter may not be negative");
-        }
-    }
-}
